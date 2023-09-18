63b4ab702cd8de6a682d12cbb0dcac2c4ab7ad79
rvandres001 <rvandres001@gmail.com>
2023-09-18 18:00:19 -0500

    inserted sh files to run selected integration test

---
+++ integrationTestRunner.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+flutter drive --driver=integration_test.dart --target=integration_test/bloc_test.dart -d macos
+flutter drive --driver=integration_test.dart --target=integration_test/cubit_test.dart -d macos
+flutter drive --driver=integration_test.dart --target=integration_test/riverpod_test.dart -d macos
+flutter drive --driver=integration_test.dart --target=integration_test/riverpod_2.0_test.dart -d macos
